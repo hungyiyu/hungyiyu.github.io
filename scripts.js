fad89a45dac380cbdc09c15f51d27e7856df710f
hungyiyu <hongyee933024@gmail.com>
2024-06-03 00:26:57 +0800

    auto experience

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
-    $('.fade-in').each(function (i) {
-        var element = $(this);
-        setTimeout(function () {
-            element.addClass('visible');
-        }, i * 500);
+    $.getJSON("experiences.json", function(data) {
+        $('#introduction').text(data.introduction);
+        
+        data.teaching.forEach(function(item) {
+            $('#teaching').append('<li class="experience-item">' + item + '</li>');
+        });
+        
+        data.research.forEach(function(item) {
+            $('#research').append('<li class="experience-item">' + item + '</li>');
+        });
+        
+        data.others.forEach(function(item) {
+            $('#others').append('<li class="experience-item">' + item + '</li>');
+        });
+        
+        $('.fade-in').each(function (i) {
+            var element = $(this);
+            setTimeout(function () {
+                element.addClass('visible');
+            }, i * 500);
+        });
     });
 });
